2e73ad948751c5d496f47134c59f2c14776ad480
Psychopixel <124247646+Psychopixel@users.noreply.github.com>
2025-01-12 11:50:02 +0100

    Project structure: src/tests folders, requirements.txt

---
+++ run_simulation.py
@@ -0,0 +1,5 @@
+# run_simulation.py
+from src.main import main
+
+if __name__ == "__main__":
+    main()
---
+++ src/main.py
@@ -0,0 +1,5 @@
+def main():
+    print("Hello AI Creature Simulator in Python!")
+
+if __name__ == "__main__":
+    main()
